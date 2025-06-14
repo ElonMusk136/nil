e81320070170f4408687bd51f6f09def629d063e
renlua <141826500+renlua@users.noreply.github.com>
2025-06-14 09:13:28 +0800

    Create 服务器列表.lua

---
+++ 服务器列表.lua
@@ -0,0 +1,13 @@
+local Server = {
+    ["被遗弃"] = "https://raw.githubusercontent.com/DevSloPo/nil/refs/heads/main/%E8%A2%AB%E9%81%97%E5%BC%83",
+    ["死铁轨"] = "https://raw.githubusercontent.com/DevSloPo/nil/refs/heads/main/%E6%AD%BB%E9%93%81%E8%BD%A8",
+    ["生存7天"] = "https://raw.githubusercontent.com/DevSloPo/nil/refs/heads/main/%E7%94%9F%E5%AD%987%E5%A4%A9",
+    ["柔术无限"] = "https://raw.githubusercontent.com/DevSloPo/nil/refs/heads/main/%E6%9F%94%E6%9C%AF%E6%97%A0%E9%99%90",
+    ["极速传奇"] = "https://raw.githubusercontent.com/DevSloPo/nil/refs/heads/main/%E6%9E%81%E9%80%9F%E4%BC%A0%E5%A5%87",
+    ["忍者元素大亨"] = "https://raw.githubusercontent.com/DevSloPo/nil/refs/heads/main/%E5%BF%8D%E8%80%85%E5%85%83%E7%B4%A0%E5%A4%A7%E4%BA%A8",
+    ["格蕾丝"] = "https://raw.githubusercontent.com/DevSloPo/nil/refs/heads/main/%E6%A0%BC%E8%95%BE%E4%B8%9D",
+    ["最强战场"] = "https://raw.githubusercontent.com/DevSloPo/nil/refs/heads/main/%E6%9C%80%E5%BC%BA%E6%88%98%E5%9C%BA",
+    ["战争大亨"] = "https://raw.githubusercontent.com/DevSloPo/nil/refs/heads/main/%E6%88%98%E4%BA%89%E5%A4%A7%E4%BA%A8",
+    ["刀刃球"] = "https://raw.githubusercontent.com/DevSloPo/nil/refs/heads/main/%E5%88%80%E5%88%83%E7%90%83",
+} -- ["Server Name"] = "URl",
+return Server
