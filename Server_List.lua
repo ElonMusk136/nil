717637ad4e9e2b1919985218c85238157f589445
renlua <141826500+renlua@users.noreply.github.com>
2025-06-20 23:51:05 +0800

    Update Server_List.lua

--- Server_List.lua
+++ Server_List.lua
@@ -12,6 +12,7 @@ local Server = {
     ["汽车经销大亨"] = "https://raw.githubusercontent.com/DevSloPo/nil/blob/main/%E6%B1%BD%E8%BD%A6%E7%BB%8F%E9%94%80%E5%A4%A7%E4%BA%A8",
     ["Hunted 丨黑暗欺骗"] = "https://raw.githubusercontent.com/DevSloPo/nil/refs/heads/main/Hunted%20%E4%B8%A8%E9%BB%91%E6%9A%97%E6%AC%BA%E9%AA%97",
     ["免费 | 终极战场"] = "https://raw.githubusercontent.com/DevSloPo/nil/refs/heads/main/%E7%BB%88%E6%9E%81%E6%88%98%E5%9C%BA%E4%B8%A8%E6%B0%B8%E4%B9%85%E5%85%8D%E8%B4%B9",
+    ["种植花园"] = "https://raw.githubusercontent.com/DevSloPo/nil/refs/heads/main/%E7%A7%8D%E6%A4%8D%E8%8A%B1%E5%9B%AD",
 } -- ["Server Name"] = "URl",
 local Other = { --通用
     ["通用"] = "https://raw.githubusercontent.com/DevSloPo/nil/refs/heads/main/%E9%80%9A%E7%94%A8",
